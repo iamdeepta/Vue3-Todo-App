774a633260a9a8bc8f05fda9b6ce96ec29ce4249
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2021-04-28 14:17:35 +0600

    Initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="main container col-md-4">
+    <div class="input">
+      <form @submit.prevent="addTask">
+        <div class="row">
+          <input
+            type="text"
+            class="task_input form-control col-md-6"
+            v-model="taskList"
+          />
+          <button class="btn btn-primary col-md-4">+</button>
+        </div>
+        <p style="color: red; font-size: 12px; margin-top: 13px">
+          {{ errors }}
+        </p>
+      </form>
+    </div>
+    <div class="todo_list" v-for="datas in todoTasks" :key="datas">
+      {{ datas.toUpperCase() }}
+    </div>
+  </div>
+</template>
+
+<script>
+//import Data from "../data.js";
+export default {
+  name: "Todos",
+  data() {
+    return {
+      taskList: null,
+      todoTasks: [],
+      errors: "",
+    };
+  },
+
+  methods: {
+    addTask() {
+      if (this.taskList != null) {
+        this.todoTasks.push(this.taskList);
+        this.taskList = null;
+
+        this.errors = "";
+      } else if (this.taskList == null) {
+        this.errors = "Please write something!";
+        setTimeout(() => {
+          this.errors = "";
+        }, 3000);
+      }
+    },
+  },
+  // updated() {
+  //   if (this.taskList == null) {
+  //     this.errors = "Please write something!";
+  //   }
+  // },
+
+  // created() {
+  //   this.todoTasks = Data;
+  // },
+};
+</script>
+
+<style></style>
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const Data = [
+  // {
+  //   id: 1,
+  //   task: "Buy a pen"
+  // },
+  // {
+  //   id: 2,
+  //   task: "Take shower"
+  // },
+  // {
+  //   id: 3,
+  //   task: "Buy Cold Juice"
+  // }
+];
+
+export default Data;
